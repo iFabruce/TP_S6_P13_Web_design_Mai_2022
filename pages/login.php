7cb1d2c8d6587b16fe6fcc2d9d90dabac08e83fd
404 <fabrucemaminiaina@gmail.com>
2022-05-17 22:11:30 +0300

    ACCUEIL_FRONTOFFICE

--- pages/login.php
+++ pages/login.php
@@ -1,70 +1,248 @@
-<?php 
-require ('inc/functions.php');
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $mail=$_POST['mail'];
-    $mdp=$_POST['mdp'];
-    $erreur='erreur';
-    $row=get_member_by_mail($mail);
-    // check the match of the passwords
-    if($row['motdepasse']!=sha1($mdp))
-    {
-        header("Location:".$_SERVER['SCRIPT_NAME']."?page=login&erreur=".$erreur);
-    }
-    if($row['motdepasse']==sha1($mdp))
-    {
-        session_start();
-        $_SESSION['user']=$row['idmembre'];
-        replace_v_m($_SESSION['user']);
-        header('Location:pages/liste_blog.php');
-    }
-    exit;
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="container">
-    
-    <div class="row">
-        
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
 
-    </div>
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <h1>BackOffice</h1><br>
-            <form  action="index.php?page=login" method="POST" >
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Login</label>
-                <input  type="text" name="mail" placeholder="Login" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-               
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" name="mdp" placeholder="mot de passe" class="form-control" id="exampleInputPassword1">
-              </div>
-              <div class="mb-3 form-check">
-    <label class="form-check-label" for="exampleCheck1"></label>
-  </div>
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
-              <button type="submit" class="btn btn-warning">Submit</button>
-            </form>
-        </div>
-        <div class="col-md-4"></div>
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"> 
 
-    </div>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
 
-</div>
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
 
+</head>
 
+<body>
    
-    <?php 
-    // if error exists
-    $erreur=isset($_GET['erreur'])? $_GET['erreur']: '';
-    	if($erreur=='erreur')
-    	{?>
-            <div class="alert alert-danger" role="alert">
-                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                <span class="sr-only">Error:</span>
-                le mot de passe est incorrecte
+    <!-- Navbar Start -->
+ <div class="container-fluid nav-bar p-0">
+    <div class="container-lg p-0">
+        <nav class="navbar navbar-expand-lg bg-secondary navbar-dark">
+            <a href="index.html" class="navbar-brand">
+                <h1 class="m-0 text-white display-4"><span class="text-primary">C</span>limat<span class="text-primary">N</span>ews</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav ml-auto py-0">
+                    <a href="/" class="nav-item nav-link active">Accueil</a>
+                    <a href="/blog" class="nav-item nav-link">Blog</a>
+                    <a href="/faq" class="nav-item nav-link">FAQ</a>
+
+                </div>
             </div>
-    <?php } ?>
+        </nav>
+    </div>
+</div>
+<!-- Navbar End -->
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5">
+        <div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#header-carousel" data-slide-to="1"></li>
+                    <li data-target="#header-carousel" data-slide-to="2"></li>
+                </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="img-fluid" src="img/bg.jpg" alt="Image">
+                    <div class="carousel-caption d-flex align-items-center justify-content-center">
+                        <div class="p-5" style="width: 100%; max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-md-3">Protegeons notre environnement</h5>
+                            <h1 class="display-3 text-white mb-md-4">L'avenir du climat entre nos mains</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="img/bg.jpg" alt="Image">
+                    <div class="carousel-caption d-flex align-items-center justify-content-center">
+                        <div class="p-5" style="width: 100%; max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-md-3">Soyez informer</h5>
+                            <h1 class="display-3 text-white mb-md-4">Système d'information climatique planétaire</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="img/bg.jpg" alt="Image">
+                    <div class="carousel-caption d-flex align-items-center justify-content-center">
+                        <div class="p-5" style="width: 100%; max-width: 900px;">
+                            <h5 class="text-white text-uppercase mb-md-3">Incitons les autres</h5>
+                            <h1 class="display-3 text-white mb-md-4">Outil de sensibilisation pour les internautes</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel End -->
+                     <!-- Features Start -->
+                     <div class="container-fluid pt-5 pb-2">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 mb-5">
+                                    <small class="bg-primary text-white text-uppercase font-weight-bold px-1">Pourquoi devrions nous s'inquiter?</small>
+                                    <h1 class="mt-2 mb-3"> 50 milliars de tonnes de déchets platstique estimé</h1>
+                                    <h4 class="font-weight-normal text-muted mb-4">Qui ne seront disparu que dans 500 ans </h4>
+                                
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="row">
+                                        <div class="col-sm-6 pb-1">
+                                            <div class="d-flex flex-column align-items-center border px-4 mb-4">
+                                                <h2 class="display-3 text-primary mb-3" data-toggle="counter-up">950</h2>
+                                                <h5 class="font-weight-bold mb-4">Membres du ClimateNews</h5>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 pb-1">
+                                            <div class="d-flex flex-column align-items-center border px-4 mb-4">
+                                                <h2 class="display-3 text-primary mb-3" data-toggle="counter-up">13</h2>
+                                                <h5 class="font-weight-bold mb-4">Pays</h5>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 pb-1">
+                                            <div class="d-flex flex-column align-items-center border px-4 mb-4">
+                                                <h2 class="display-3 text-primary mb-3" data-toggle="counter-up">140</h2>
+                                                <h5 class="font-weight-bold mb-4">Initiatives terminées</h5>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 pb-1">
+                                            <div class="d-flex flex-column align-items-center border px-4 mb-4">
+                                                <h2 class="display-3 text-primary mb-3" data-toggle="counter-up">8</h2>
+                                                <h5 class="font-weight-bold mb-4">Projets en cours</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Features End -->
+
+    <!-- About Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="row align-items-center pb-1">
+                <div class="col-lg-5">
+                    <img class="img-thumbnail p-3" src="img/about.jpg" alt="">
+                </div>
+                <div class="col-lg-7 mt-5 mt-lg-0">
+                    <small class="bg-primary text-white text-uppercase font-weight-bold px-1">Quel est notre objectif?</small>
+                    <h1 class="mt-2 mb-4">Etre informé sur le climat et de son réchauffement</h1>
+                    <p class="mb-4">Nous partageons des informations de qualités pour permettre à la population mondiale d'être au courant du réchauffement climatique,de ses causes ainsi que ces conséquences.</p>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="d-flex align-items-center border mb-4 mb-lg-0 p-4" style="height: 120px;">
+                        <i class="fa fa-2x fa-map-marker-alt text-primary mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="font-weight-bold">Notre siège</h5>
+                            <p class="m-0">102 Andoharanofotsy, Antananarivo Madagascar</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="d-flex align-items-center border mb-4 mb-lg-0 p-4" style="height: 120px;">
+                        <i class="fa fa-2x fa-envelope-open text-primary mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="font-weight-bold">Email</h5>
+                            <p class="m-0">climatenews@info.mg</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="d-flex align-items-center border mb-4 mb-lg-0 p-4" style="height: 120px;">
+   
+                        <i class="fas fa-2x fa-phone-alt text-primary mr-3"></i>
+                        <div class="d-flex flex-column">
+                            <h5 class="font-weight-bold">Contact</h5>
+                            <p class="m-0">+0261341203517</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+
+   <!-- Footer Start -->
+   <div class="container-fluid bg-secondary text-white mt-5 pt-5 px-sm-3 px-md-5">
+    <div class="row pt-5">
+        <div class="col-lg-3 col-md-6 mb-5">
+            <a href="index.html" class="navbar-brand">
+                <h1 class="m-0 text-white display-4"><span class="text-primary">C</span>limat<span class="text-primary"><br> N</span>ews</h1>
+            </a>
+         
+            <div class="d-flex justify-content-start mt-4">
+                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-primary rounded-circle text-center mr-2 px-0" style="width: 38px; height: 38px;" href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h5 class="font-weight-bold text-primary mb-4">Liens rapides</h5>
+            <div class="d-flex flex-column justify-content-start">
+                <a class="text-white mb-2" href="/"><i class="fa fa-angle-right text-primary mr-2"></i>Accueil</a>
+                <a class="text-white mb-2" href="/blog"><i class="fa fa-angle-right text-primary mr-2"></i>Blog</a>
+                <a class="text-white mb-2" href="/faq"><i class="fa fa-angle-right text-primary mr-2"></i>FAQ</a>
+           
+            </div>
+        </div>
+       
+        <div class="col-lg-3 col-md-6 mb-5">
+            <h5 class="font-weight-bold text-primary mb-4">Soyez à jour</h5>
+            <p>Nous sommes disponibles 24h/24 et 7j/7 pour toute information complémentaire.</p>
+            <p><i class="fa fa-map-marker-alt text-primary mr-2"></i>102 Andoharanofotsy, Antananarivo Madagascar</p>
+            <p><i class="fa fa-phone-alt text-primary mr-2"></i>+0261341203517</p>
+            <p><i class="fa fa-envelope text-primary mr-2"></i>climatenews@info.mg</p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid py-4 px-sm-3 px-md-5">
+    <p class="m-0 text-center">
+        &copy; <a class="font-weight-semi-bold" href="#">ClimateNEWS</a>.Tous droits réservés
+ 
+    </p>
+</div>
+<!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary back-to-top"><i class="fa fa-angle-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
